a255ec319024daa090e7d22f39724778492eb68c
jvanwin <vanwi@DESKTOP-3VH0G19>
2018-11-05 15:40:59 -0500

    Added boilerplate to MachineInput.
    Created a complementary enum SCORE_TYPE.

--- CSE 201 Group Project/src/MachineInput.java
+++ CSE 201 Group Project/src/MachineInput.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
 public class MachineInput implements PlayerInput
 {
 	// Moves possible for AI only for bins 7 - 12.
@@ -5,9 +9,14 @@ public class MachineInput implements PlayerInput
 	
 	private final int[] ALL_MOVES_POSSIBLE =
 	{ 7, 8, 9, 10, 11, 12 };
+	private Map<SCORE_TYPE, Double> biasFromType;
+	
+	
 	
 	public MachineInput()
 	{
+		biasFromType = new HashMap<>();
+		biasFromType.put(SCORE_TYPE.CAN_LAND_IN_MANCALA, 1.0);
 	}
 	
 	public int getMove(Board boardState)
@@ -18,7 +27,8 @@ public class MachineInput implements PlayerInput
 		for(int i = 0; i < ALL_MOVES_POSSIBLE.length; ++i)
 		{
 			double weight = getWeight(ALL_MOVES_POSSIBLE[i], boardState);
-			if(weight > max) {
+			if(weight > max)
+			{
 				maxIndex = i;
 				max = weight;
 			}
@@ -29,7 +39,18 @@ public class MachineInput implements PlayerInput
 	
 	// TODO Finish method
 	private double getWeight(int move, Board boardState)
-	{	
+	{
+		
 		return Math.random();
+	}
+	
+	public static double canLandInMancala(Integer move, Board boardState)
+	{
+		return 0.0; // TODO
+	}
+	
+	public static double captureMade(Integer move, Board boardState)
+	{
+		return 0.0; //TODO
 	}
 }
