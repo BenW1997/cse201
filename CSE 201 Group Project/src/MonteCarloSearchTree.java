ba7dce916b9cc57d1f6690e96004ec126bcba3fb
jvanwin <vanwi@DESKTOP-3VH0G19>
2018-11-23 22:22:56 -0500

    multiple changes
    -moved MonteCarloSearchTree to own file
    -added toString() override to search tree
    -new methods bestChild() and expand()

--- CSE 201 Group Project/src/MonteCarloSearchTree.java
+++ CSE 201 Group Project/src/MonteCarloSearchTree.java
@@ -1,16 +1,52 @@
+import java.util.List;
+import java.util.Objects;
 
 public class MonteCarloSearchTree
 {
 	// Exploration parameter - weight given to exploring less explored moves
+	// Best value found through experimentation
 	private final double EXPLORATION = Math.sqrt(2);
 	
-	private Node<MonteCarloStats<Integer>> root;
+	private Node<MonteCarloStats> root;
 	private GameState gamePrototype;
 	
 	public MonteCarloSearchTree(GameState game)
 	{
 		this.gamePrototype = game;
-		this.root = new Node<>(new MonteCarloStats<>(game.whoseTurn(), null)); //TODO
+		this.root = new Node<>(new MonteCarloStats(gamePrototype));
+		expand(this.root);
+	}
+	
+	public Node<MonteCarloStats> bestChild()
+	{
+		Node<MonteCarloStats> best = null;
+		
+		for(Node<MonteCarloStats> n : root.getChildren())
+		{
+			if(best == null)
+			{
+				best = n;
+				continue;
+			}
+			
+			if(n.getData().compareTo(best.getData()) > 1)
+			{
+				best = n;
+			}
+		}
+		
+		return best;
+	}
+	
+	private void expand(Node<MonteCarloStats> node)
+	{
+		List<Integer> validMoves = node.getData().game().validMoves();
+		for(Integer i : validMoves)
+		{
+			GameState nextGame = new GameState(node.getData().game());
+			nextGame.move(i);
+			node.addChild(new MonteCarloStats(nextGame, i));
+		}
 	}
 	
 	/**
@@ -26,52 +62,21 @@ public class MonteCarloSearchTree
 	 */
 	public final double uct(int w_i, int n_i, int t, double c)
 	{
+		assert (w_i <= n_i);
+		assert (n_i <= t);
+		
 		return ((double) w_i) / ((double) n_i)
 				+ c * Math.sqrt(Math.log(t) / n_i);
 	}
 	
-	private class MonteCarloStats<T>
+	public String toString()
 	{
-		private int wins = 0;
-		private int visits = 0;
-		private final Player player;
-		private final T move; // move which caused this node
+		StringBuilder sb = new StringBuilder();
 		
-		public MonteCarloStats(Player p, T move)
-		{
-			this.player = p;
-			this.move = move;
-		}
+		sb.append("Root Game:").append(System.lineSeparator())
+				.append(gamePrototype);
+		sb.append("Tree:").append(System.lineSeparator()).append(root);
 		
-		public void addWin()
-		{
-			wins++;
-			visits++;
-		}
-		
-		public void addLoss()
-		{
-			visits++;
-		}
-		
-		public int wins()
-		{
-			return wins;
-		}
-		
-		public int visits()
-		{
-			return visits;
-		}
-		
-		public Player player()
-		{
-			return player;
-		}
-		
-		public T move()
-		{
-			return move;
-		}
+		return sb.toString();
 	}
 }
---
+++ CSE 201 Group Project/src/MonteCarloStats.java
@@ -0,0 +1,88 @@
+
+public class MonteCarloStats implements Comparable<MonteCarloStats>
+	{
+		private int wins = 0;
+		private int visits = 0;
+		private GameState game;
+		private Integer move;
+		
+		public MonteCarloStats(GameState game)
+		{
+			this.game = game;
+		}
+		
+		public MonteCarloStats(GameState game, Integer move)
+		{
+			this.game = game;
+			this.move = move;
+		}
+		
+		public void addWin()
+		{
+			wins++;
+			visits++;
+		}
+		
+		public void addLoss()
+		{
+			visits++;
+		}
+		
+		public void setWins(int wins)
+		{
+			this.wins = wins;
+		}
+		
+		public void setVisits(int visits)
+		{
+			this.visits = visits;
+		}
+		
+		public void setMove(Integer move)
+		{
+			this.move = move;
+		}
+		
+		public int wins()
+		{
+			return wins;
+		}
+		
+		public int visits()
+		{
+			return visits;
+		}
+		
+		public Integer move()
+		{
+			return move;
+		}
+		
+		public GameState game()
+		{
+			return game;
+		}
+		
+		public double ratio()
+		{
+			return (double) wins / (double) visits;
+		}
+		
+		public String toString()
+		{
+			StringBuilder sb = new StringBuilder();
+			
+			sb.append("(Wins: ").append(wins);
+			sb.append(", Visits: ").append(visits);
+			sb.append(", Player: ").append(game.whoseTurn());
+			sb.append(", Move: ").append(move);
+			sb.append(")");
+			
+			return sb.toString();
+		}
+		
+		public int compareTo(MonteCarloStats arg0)
+		{
+			return Double.compare(this.visits(), arg0.visits());
+		}
+	}
